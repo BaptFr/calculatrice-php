00543cbbdd7515a381f9082547c21753c63bb909
BaptFR <baptistesalazar@gmail.com>
2025-04-28 16:34:20 +0200

    Style: façade calculatrice

--- index.php
+++ index.php
@@ -5,55 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
 </head>
 <body> 
     <header>
         <nav class="flex flex-row items-center justify-center">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">NAV</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav"></ul>
+            <div class="p-5 container-fluid">
+                <h1> CALCULATRICE </h1>
             </div>
         </nav>
     </header>
-    <main class=" p-8 w-full flex flex-col justify-center items-center">
-        <h1 class="items-center text-center">CALCULATRICE</h1>
+    <main class=" px-8 py-3 w-full flex flex-col justify-center items-center border border-gray-1000">
         <div class= "w-full d-flex flex-col justify-center items-center">
             <div class=" flex flex-col justify-center items-center">
-                <div class="h-75 w-full border border-gray-700 text-center bg-[#636363]" >
-                    ecran
-                </div>
-                <div class="w-full flex flex-row justify-between p-10 gap-10">
-                    <div class="w-75 flex flex-col border-gray-700 gap-5"> 
-                        <ul class= " flex flex-row justify-between gap-7 ">
-                            <li class="border border-gray-700- p-10">1</li>
-                            <li class="border border-gray-700- p-10">2</li>
-                            <li class="border border-gray-700- p-10">3</li>
-                        </ul>
-                        <ul class= " flex flex-row justify-between gap-7 ">
-                            <li class="border border-gray-700- p-10">4</li>
-                            <li class="border border-gray-700- p-10">5</li>
-                            <li class="border border-gray-700- p-10">6</li>
-                        </ul>
-                        <ul class= " flex flex-row justify-between gap-7 ">
-                            <li class="border border-gray-700- p-10">7</li>
-                            <li class="border border-gray-700- p-10">8</li>
-                            <li class="border border-gray-700- p-10">9</li>
-                        </ul>
-                        <li class="border border-gray-700- p-10">0</li>
+                <div class="container-ecran" >
+                     <div class= "w-75 font-extrabold text-9xl">
+                        7
                     </div>
-                    <div class=" w-20 flex flex-col gap-5"> 
-                        <ul  class=" w-full flex flex-row justify-center gap-5">
-                            <li class="border border-gray-700- p-[40px] m-auto">+</li>
-                            <li class="border border-gray-700- p-[40px] m-auto">-</li>
+                    <div class= "w-25 font-extrabold text-5xl flex flex-row">
+                        3 + 4
+                    </div>
+                 
+                </div>
+                <div class="w-full flex flex-row justify-between p-2 flew-wrap">
+                    <div class=" container-chiffres w-75 flex flex-col border-gray-700 gap-2"> 
+                        <ul class= " flex flex-row justify-between">
+                            <button class="bouton-classique">7</button> 
+                            <button class="bouton-classique">8</button> 
+                            <button class="bouton-classique">9</button> 
                         </ul>
-                        <ul  class=" w-full flex flex-row justify-center gap-5">
-                            <li class="border border-gray-700- p-[40px] m-auto">*</li>
-                            <li class="border border-gray-700- p-[40px] m-auto">/</li> 
+                        <ul class= " flex flex-row justify-between">
+                            <button class="bouton-classique">4</button>
+                            <button class="bouton-classique">5</button> 
+                            <button class="bouton-classique">6</button> 
                         </ul>
+                    
+                        <ul class= " flex flex-row justify-between">
+                            <button class="bouton-classique">1</button> 
+                            <button class="bouton-classique">2</button> 
+                            <button class="bouton-classique">3</button> 
+                        </ul>
+                        
+                        <ul class=" flex flex-row justify-between">
+                            <button class="bouton-classique">0</button>
+                            <button class="bouton-classique ">CE</button>
+                            <button class="bouton-classique ">C</button>
+                        </ul>
+                    </div>
+                    <div class="container-signes flex flex-col gap-4"> 
+                        <ul  class="flex flex-row gap-2">
+                            <button class="bouton-grand">+</button>
+                            <button class="bouton-grand">-</button>
+                        </ul>
+                        <ul  class=" flex flex-row gap-2">
+                            <button class=" bouton-grand">*</button>
+                            <button class="bouton-grand">/</button> 
+                        </ul>
+                        <button class=" bouton-grand-double ">=</button>
                     </div>
                 </div> 
             </div>   
